394fe3bb7a79c018f66a1cd40c370f64ed4130b5
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-15 18:09:17 +0100

    feat: base

---
+++ js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// function
+function createCell (tag, className, content) {
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    element.append(content);
+
+    return element;
+}
+
+// /function
+
+
+const contenitoreGame = document.querySelector('.cont-game');
+const bottone = document.querySelector('.btn');
+
+for (let i = 1; i <= 100; i++) {
+
+    bottone.addEventListener('click', function() {
+    
+    contenitoreGame.append(cell);
+})
+const cell = createCell('div', 'cell', i )
+    
+    
+}
+
+
