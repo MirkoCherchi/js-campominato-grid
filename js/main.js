77d6898070e19c52af45f9aeb2422956b69120ac
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-16 16:36:43 +0100

    feat: completato

--- js/main.js
+++ js/main.js
@@ -1,36 +1,78 @@
 'use strict';
 
-// function
-function createCell (tag, className, content) {
-    const element = document.createElement(tag);
-    element.classList.add(className);
-    element.append(content);
+/*
+Funzioni
+*/
 
-    return element;
+function creaElementi(tag, className, content) {
+  const element = document.createElement(tag);
+  element.classList.add(className);
+  element.append(content);
+
+  return element;
 }
 
-// /function
+function selectCellNumber(level) {
+  let cellNumber;
+  switch (level) {
+    case 2:
+      cellNumber = 81;
+      break;
 
+    case 3:
+      cellNumber = 49;
+      break;
 
-const contenitoreGame = document.querySelector('.cont-game');
-const bottone = document.querySelector('.btn');
+    case 1:
+    default:
+      cellNumber = 100;
+      break;
+  }
 
-for (let i = 1; i <= 100; i++) {
-
-    bottone.addEventListener('click', function() {
-    
-    contenitoreGame.append(cell);
-    
-})
-const cell = createCell('div', 'cell', i )
-
-cell.addEventListener('click', function(){
-    cell.classList.add('click');
-    console.log(i);
-})
-
-    
-    
+  return cellNumber;
 }
+
+function creaContCell(mainElement, cellNumber) {
+  const cell = Math.sqrt(cellNumber)
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 1; i <= cellNumber; i++) {
+    const elementiCreati = creaElementi('div', 'cell', i);
+    elementiCreati.classList.add(`cell-${cell}`);
+
+    elementiCreati.addEventListener('click', function () {
+      console.log(elementiCreati.innerHTML);
+      elementiCreati.classList.add('click');
+    });
+
+    fragment.append(elementiCreati);
+  }
+  mainElement.append(fragment);
+}
+
+function contGame() {
+    resetGame();
+  const cont = document.querySelector('.cont-game');
+  const levelSelect = document.getElementById('difficult');
+  const level = parseInt(levelSelect.value);
+  const cellNumber = selectCellNumber(level);
+
+  console.log(cellNumber);
+
+  creaContCell(cont, cellNumber);
+}
+
+function resetGame() {
+  const board = document.querySelector('.cont-game');
+  board.innerHTML = '';
+}
+
+
+/*
+Gioco
+*/
+
+const inizioGioco = document.querySelector('.btn');
+inizioGioco.addEventListener('click', contGame);
 
 
